c16bd831cebf936f7cbee2dd466b44473c000deb
Xuee0904 <cedric2827@gmail.com>
2025-09-15 21:40:12 +0800

    Added exception and try-catch statements

--- 03-Laboratory-Exercise-2-Madayag/Inventory.cs
+++ 03-Laboratory-Exercise-2-Madayag/Inventory.cs
@@ -14,6 +14,30 @@ namespace _03_Laboratory_Exercise_2_Madayag
 {
     public partial class Inventory : Form
     {
+        public class NumberFormatException : Exception
+        {
+            public NumberFormatException(string message) : base(message)
+            {
+
+            }
+        }
+
+        public class StringFormatException : Exception
+        {
+            public StringFormatException(string message) : base(message)
+            {
+
+            }
+        }
+
+        public class CurrencyFormatException : Exception
+        {
+            public CurrencyFormatException(string message) : base(message)
+            {
+
+            }
+        }
+
         public Inventory()
         {
             InitializeComponent();
@@ -59,21 +83,21 @@ namespace _03_Laboratory_Exercise_2_Madayag
         {
             if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
                 //Exception here
-                throw new Exception("Invalid Name Format");
+                throw new StringFormatException("Invalid Name Format");
                 return name;
         }
         public int Quantity(string qty)
         {
             if (!Regex.IsMatch(qty, @"^[0-9]"))
                 //Exception here
-                throw new Exception("Invalid Number Format");
+                throw new NumberFormatException("Invalid Number Format");
             return Convert.ToInt32(qty);
         }
         public double SellingPrice(string price)
         {
             if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
                 //Exception here
-                throw new Exception("Invalid Currency Format");
+                throw new CurrencyFormatException("Invalid Currency Format");
             return Convert.ToDouble(price);
         }
 
@@ -91,17 +115,32 @@ namespace _03_Laboratory_Exercise_2_Madayag
 
         private void AddProductButton_Click(object sender, EventArgs e)
         {
-            _ProductName = Product_Name(ProductNameText.Text);
-            _Category = CategoryCB.Text;
-            _MfgDate = MfgDatePicker.Value.ToString("yyyy-MM-dd");
-            _ExpDate = ExpDatePicker.Value.ToString("yyyy-MM-dd");
-            _Description = DescriptionRichText.Text;
-            _Quantity = Quantity(QuantityText.Text);
-            _SellPrice = SellingPrice(SellPriceText.Text);
-            showProductList.Add(new ProductClass(_ProductName, _Category, _MfgDate,
-            _ExpDate, _SellPrice, _Quantity, _Description));
-            ViewProductListGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-            ViewProductListGrid.DataSource = showProductList;
+            try
+            {
+                _ProductName = Product_Name(ProductNameText.Text);
+                _Category = CategoryCB.Text;
+                _MfgDate = MfgDatePicker.Value.ToString("yyyy-MM-dd");
+                _ExpDate = ExpDatePicker.Value.ToString("yyyy-MM-dd");
+                _Description = DescriptionRichText.Text;
+                _Quantity = Quantity(QuantityText.Text);
+                _SellPrice = SellingPrice(SellPriceText.Text);
+                showProductList.Add(new ProductClass(_ProductName, _Category, _MfgDate,
+                _ExpDate, _SellPrice, _Quantity, _Description));
+                ViewProductListGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+                ViewProductListGrid.DataSource = showProductList;
+            }
+            catch (StringFormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (NumberFormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            catch (CurrencyFormatException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
